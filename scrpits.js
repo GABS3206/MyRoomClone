7e11828e5a4672ad96c7aab8959aac22a9ed11a5
Gabrielle <GErves2020@stanner.org>
2019-08-12 15:18:32 -0400

    new pictures

--- scrpits.js
+++ scrpits.js
@@ -23,10 +23,10 @@ interact('.draggable')
       textEl && (textEl.textContent =
         'moved a distance of ' +
         (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                   Math.pow(event.pageY - event.y0, 2) | 0))
+                  Math.pow(event.pageY - event.y0, 2) | 0))
           .toFixed(2) + 'px')
     }
-  })
+  })//
 
 function dragMoveListener (event) {
   var target = event.target
@@ -46,3 +46,52 @@ function dragMoveListener (event) {
 
 // this is used later in the resizing and gesture demos
 window.dragMoveListener = dragMoveListener
+
+interact('.resize-drag')
+  .draggable({
+    onmove: window.dragMoveListener,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent'
+      })
+    ]
+  })
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
+
+    modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'parent',
+        endOnly: true
+      }),
+
+      // minimum size
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 }
+      })
+    ],
+
+    inertia: true
+  })
+  .on('resizemove', function (event) {
+    var target = event.target
+    var x = (parseFloat(target.getAttribute('data-x')) || 0)
+    var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+    // update the element's style
+    target.style.width = event.rect.width + 'px'
+    target.style.height = event.rect.height + 'px'
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left
+    y += event.deltaRect.top
+
+    target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px,' + y + 'px)'
+
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+    target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+  })
