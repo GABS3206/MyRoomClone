6861e5f20f1cea890fd916b13ac53600bcd63f8f
GABS3206 <53828401+GABS3206@users.noreply.github.com>
2021-10-19 14:49:37 -0400

    Update scrpits.js

--- scrpits.js
+++ scrpits.js
@@ -1,46 +1,32 @@
-/* The dragging code for '.draggable' from the demo above
- * applies to this demo as well so it doesn't have to be repeated. */
- interact('.draggable')
-   .draggable({
-     // enable inertial throwing
-     inertia: true,
-     // keep the element within the area of it's parent
-     modifiers: [
-       interact.modifiers.restrictRect({
-         restriction: '.row',
-         endOnly: true
-       })
-     ],
-     // enable autoScroll
-     autoScroll: true,
+// target elements with the "draggable" class
 
-     // call this function on every dragmove event
-     onmove: dragMoveListener,
-     // call this function on every dragend event
-     onend: function (event) {
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: true,
 
-     }
-   })
-// enable draggables to be dropped into this
-interact('.dropzone').dropzone({
-  // only accept elements matching this CSS selector
-  accept: '#drag-1',
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.20,
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p')
 
-  // listen for drop related events:
-
-  ondropactivate: function (event) {
-  },
-  ondragenter: function (event) {
-  },
-  ondragleave: function (event) {
-  },
-  ondrop: function (event) {
-  },
-  ondropdeactivate: function (event) {
-  }
-})
+      textEl && (textEl.textContent =
+        'moved a distance of ' +
+        (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                  Math.pow(event.pageY - event.y0, 2) | 0))
+          .toFixed(2) + 'px')
+    }
+  })//
 
 function dragMoveListener (event) {
   var target = event.target
@@ -58,4 +44,54 @@ function dragMoveListener (event) {
   target.setAttribute('data-y', y)
 }
 
+// this is used later in the resizing and gesture demos
 window.dragMoveListener = dragMoveListener
+
+interact('.resize-drag')
+  .draggable({
+    onmove: window.dragMoveListener,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent'
+      })
+    ]
+  })
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
+
+    modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'parent',
+        endOnly: true
+      }),
+
+      // minimum size
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 }
+      })
+    ],
+
+    inertia: true
+  })
+  .on('resizemove', function (event) {
+    var target = event.target
+    var x = (parseFloat(target.getAttribute('data-x')) || 0)
+    var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+    // update the element's style
+    target.style.width = event.rect.width + 'px'
+    target.style.height = event.rect.height + 'px'
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left
+    y += event.deltaRect.top
+
+    target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px,' + y + 'px)'
+
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+    target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+  })
